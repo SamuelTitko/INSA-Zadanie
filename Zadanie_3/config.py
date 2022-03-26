10af2677c8ab5b19641d6baba552097a3ece2188
Samuel Titko <samkotitko@gmail.com>
2022-03-26 20:48:49 +0100

    adding 2 another columns

--- Zadanie_3/config.py
+++ Zadanie_3/config.py
@@ -3,11 +3,11 @@ from sklearn.ensemble import RandomForestClassifier
 
 PATH_TO_DATASET = 'dataset/dataset.csv'
 
-TEST_SIZE = 0.2
+TEST_SIZE = 0.1
 
 NUMERICAL_FEATURES = ['Age']
 
-CATEGORICAL_FEATURES = ['Cabin', 'Pclass','Sex',]
+CATEGORICAL_FEATURES = ['Cabin', 'Pclass','Sex', 'Parch', 'SibSp']
 
 FEATURES = NUMERICAL_FEATURES + CATEGORICAL_FEATURES
 
